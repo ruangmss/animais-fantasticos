d6fb2e31911c89cb787087769d70f8d9c184af84
Ruan Gomes <ruan.gmss@outlook.com>
2026-04-24 15:12:27 -0300

    fix: Correção de posicionamento de span na accordion list

--- src/js/components/Contact/Contact.js
+++ src/js/components/Contact/Contact.js
@@ -4,10 +4,12 @@ export function Contact() {
       <div class="contact-title">
         <h2>Contato</h2>
       </div>
+      
       <div class="contact-content">
         <div class="contact-map">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d113854.22721293931!2d-48.7763854063353!3d-26.905253250572432!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94d8cc949bf3cf61%3A0xef02eadd26842207!2sItaja%C3%AD%20-%20Volta%20de%20Cima%2C%20Itaja%C3%AD%20-%20SC!5e0!3m2!1spt-BR!2sbr!4v1776742146427!5m2!1spt-BR!2sbr" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="Mapa de Itajaí, município localizado em Santa Catarina"></iframe>
         </div> 
+
         <div class="contact-data">
           <ul>
             <li><a href=mailto:ruan.gmss@outlook.com>ruan.gmss@outlook.com</a></li>
