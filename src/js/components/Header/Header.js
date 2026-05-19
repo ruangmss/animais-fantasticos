340987880b24464d9ed56d506385f94a81fc1f39
Ruan Gomes <ruan.gmss@outlook.com>
2026-05-19 16:07:43 -0300

    refactor: Ajuste no dropdown do menu

--- src/js/components/Header/Header.js
+++ src/js/components/Header/Header.js
@@ -8,11 +8,11 @@ export function Header() {
           <li class="dropdown">
             <a class="header-link" href="#faq">FAQ</a>
             <ul class="dropdown-list">
-              <li class="dropdown-item"><a href="#">São realmente fantásticos?</a></li>
-              <li class="dropdown-item"><a href="#">São raros?</a></li>
-              <li class="dropdown-item"><a href="#">Qual é a média de idade?</a></li>
-              <li class="dropdown-item"><a href="#">Como proteger?</a></li>
-              <li class="dropdown-item"><a href="#">Como contribuir?</a></li>
+              <li class="dropdown-item"><a href="#faq">São realmente fantásticos?</a></li>
+              <li class="dropdown-item"><a href="#faq">São raros?</a></li>
+              <li class="dropdown-item"><a href="#faq">Qual é a média de idade?</a></li>
+              <li class="dropdown-item"><a href="#faq">Como proteger?</a></li>
+              <li class="dropdown-item"><a href="#faq">Como contribuir?</a></li>
             </ul>
           </li>
           <li><a class="header-link" href="#contact">CONTATO</a></li>
@@ -24,17 +24,35 @@ export function Header() {
 }
 
 export function initDropDownMenu() {
-  const dropdownMenus = document.querySelectorAll(".dropdown");
+  const dropdownMenus = document.querySelectorAll('.dropdown');
 
   if (dropdownMenus.length) {
     dropdownMenus.forEach((dropdown) => {
-      dropdown.addEventListener("click", showDropdown);
-      dropdown.addEventListener("touchstart", showDropdown); // Padrão de click sem delay para mobile, pois no mobile os browsers demoram 300ms para adicionarem o evento
+      dropdown.addEventListener('click', manipulateDropdown);
     });
 
-    function showDropdown(event) {
-      const dropdownList = this.querySelector(".dropdown-list");
-      dropdownList.classList.toggle("active");
+    function manipulateDropdown(event) {
+      if (window.innerWidth < 768) {
+        return;
+      }
+
+      const dropdown = event.currentTarget;
+      const dropdownList = dropdown.querySelector('.dropdown-list');
+      const insideDropdownList = dropdownList.querySelectorAll('a[href^="#"]');
+
+      if (!dropdownList.contains(event.target)) {
+        event.preventDefault();
+      }
+
+      dropdownList.classList.toggle('active');
+
+      function closeDropdown(event) {
+        if (!dropdown.contains(event.target)) {
+          dropdownList.classList.remove('active');
+          document.removeEventListener('click', closeDropdown); // Quando o dropdown é fechado, seu listener do HTML é removido para que não ocorra leak
+        }
+      }
+      document.addEventListener('click', closeDropdown);
     }
   }
 }
