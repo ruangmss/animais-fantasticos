81f031bc542bb640eedab462b1cbb3f74282d7c3
Ruan Gomes <ruan.gmss@outlook.com>
2026-05-20 17:29:05 -0300

    feat: Estruturação inicial do menu mobile

--- src/js/components/Header/Header.js
+++ src/js/components/Header/Header.js
@@ -2,9 +2,11 @@ export function Header() {
   return /* html */ `
     <div class="header-bg scroll">
       <nav class="header container">
+        <button class="header-button" aria-expanded="false">MENU<span class="header-button-symbol">☰</span></button>
         <ul class="header-list">
           <li><a class="header-link" href="#animals">ANIMAIS</a></li>
           <li><a class="header-link" href="#about">SOBRE</a></li>
+
           <li class="dropdown">
             <a class="header-link" href="#faq">FAQ</a>
             <ul class="dropdown-list">
@@ -15,6 +17,7 @@ export function Header() {
               <li class="dropdown-item"><a href="#faq">Como contribuir?</a></li>
             </ul>
           </li>
+
           <li><a class="header-link" href="#contact">CONTATO</a></li>
           <li><a class="header-link" href="#" id="login">LOGIN<span>→</span></a></li>
         </ul>
@@ -54,5 +57,66 @@ export function initDropDownMenu() {
       }
       document.addEventListener('click', closeDropdown);
     }
+  }
+}
+
+export function initMobileMenu() {
+  const headerButton = document.querySelector('.header-button');
+  const symbol = headerButton.querySelector('.header-button-symbol');
+  const headerList = document.querySelector('.header-list');
+
+  if (headerButton && symbol && headerList) {
+    function manipulateMobileMenu() {
+      const headerListElements = headerList.querySelectorAll('*');
+      const ariaExpanded = headerButton.getAttribute('aria-expanded');
+
+      if (headerListElements.length && ariaExpanded) {
+        if (ariaExpanded === 'false') {
+          headerButton.setAttribute('aria-expanded', 'true');
+        } else {
+          headerButton.setAttribute('aria-expanded', 'false');
+        }
+
+        headerList.classList.toggle('mobile');
+        symbol.classList.toggle('mobile');
+
+        headerListElements.forEach((element) => {
+          element.classList.toggle('mobile');
+        });
+
+        function closeMobileMenu(event) {
+          if (!headerList.contains(event.target) && !headerButton.contains(event.target)) {
+            event.stopPropagation(); // Impossibilita a propagação do bubble, evitando que o clique suba para o HTML
+
+            headerList.classList.remove('mobile');
+            symbol.classList.remove('mobile');
+            headerButton.setAttribute('aria-expanded', 'false');
+
+            headerListElements.forEach((element) => {
+              element.classList.remove('mobile');
+            });
+            document.documentElement.removeEventListener('click', closeMobileMenu);
+          }
+        }
+        document.documentElement.addEventListener('click', closeMobileMenu);
+
+        function fixResizeLayout() {
+          const width = window.innerWidth;
+
+          if (width > 768) {
+            headerList.classList.remove('mobile');
+            symbol.classList.remove('mobile');
+            headerButton.setAttribute('aria-expanded', 'false');
+
+            headerListElements.forEach((element) => {
+              element.classList.remove('mobile');
+            });
+          }
+        }
+        window.addEventListener('resize', fixResizeLayout);
+      }
+    }
+
+    headerButton.addEventListener('click', manipulateMobileMenu);
   }
 }
--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -1,27 +1,27 @@
-import { Header, initDropDownMenu } from "../components/Header/Header.js";
-import { Footer } from "../components/Footer/Footer.js";
-import { Animals, initTabMenu } from "../components/Animals/Animals.js";
+import { Header, initDropDownMenu, initMobileMenu } from '../components/Header/Header.js';
+import { Footer } from '../components/Footer/Footer.js';
+import { Animals, initTabMenu } from '../components/Animals/Animals.js';
 import {
   FrequentlyAskedQuestions,
   initAccordionList,
-} from "../components/FrequentlyAskedQuestions/FrequentlyAskedQuestions.js";
-import { About } from "../components/About/About.js";
-import { Contact } from "../components/Contact/Contact.js";
-import { initSmoothReveal } from "../modules/smoothReveal.js";
-import { Modal, initModal } from "../components/Modal/Modal.js";
+} from '../components/FrequentlyAskedQuestions/FrequentlyAskedQuestions.js';
+import { About } from '../components/About/About.js';
+import { Contact } from '../components/Contact/Contact.js';
+import { initSmoothReveal } from '../modules/smoothReveal.js';
+import { Modal, initModal } from '../components/Modal/Modal.js';
 
 export function Home() {
-  let header = document.getElementById("header");
-  let footer = document.getElementById("footer");
-  let app = document.getElementById("app");
+  let header = document.getElementById('header');
+  let footer = document.getElementById('footer');
+  let app = document.getElementById('app');
 
   if (header && footer && app) {
     header.innerHTML = Header();
     footer.innerHTML = Footer();
-    app.innerHTML =
-      Animals() + About() + FrequentlyAskedQuestions() + Contact() + Modal();
+    app.innerHTML = Animals() + About() + FrequentlyAskedQuestions() + Contact() + Modal();
     initTabMenu();
     initDropDownMenu();
+    initMobileMenu();
     initAccordionList();
     initSmoothReveal();
     initModal();
