3d64589697b3abba402fe68921a289c2c7313cad
Ruan Gomes <ruan.gmss@outlook.com>
2026-05-27 14:50:43 -0300

    refactor: Remoção de animação ao scroll no footer

--- src/js/components/Footer/Footer.js
+++ src/js/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 export function Footer() {
   return `
-    <div class="footer-bg scroll">
+    <div class="footer-bg">
       <div class="footer container">
         <p class="footer-copy">© 2026 Animais Fantásticos. Todos os direitos reservados.</p>
       </div>
