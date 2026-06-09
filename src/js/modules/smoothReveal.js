23e52e92638d4b6ec1a8b8a743a34b05d70473f6
Ruan Gomes <ruan.gmss@outlook.com>
2026-06-09 14:02:47 -0300

    refactor: Melhoria na função de revelação suave de seções para evitar acúmulo de listeners

--- src/js/modules/smoothReveal.js
+++ src/js/modules/smoothReveal.js
@@ -1,24 +1,30 @@
 export function initSmoothReveal() {
-  const elementsToScroll = document.querySelectorAll('.scroll');
+  let elementsToScroll = Array.from(document.querySelectorAll('.scroll')); // Converte em array para ser possível utilizar o .filter
 
   if (elementsToScroll.length) {
     function smoothReveal() {
-      elementsToScroll.forEach((element) => {
-        let imaginaryLine = 0;
+      elementsToScroll = elementsToScroll.filter((element) => {
+        const imaginaryLine = element.classList.contains('footer-bg')
+          ? window.innerHeight * 0.97
+          : window.innerHeight * 0.9;
 
         const elementTop = element.getBoundingClientRect().top;
-        if (element.classList.contains('footer-bg')) {
-          imaginaryLine = window.innerHeight * 0.97;
-        } else {
-          imaginaryLine = window.innerHeight * 0.9; // Quando o elemento chega em 90% de distância do topo da tela é exibido, ou seja, numa linha imáginária 10% acima da borda inferior visível
-        }
 
         if (elementTop <= imaginaryLine) {
           element.classList.add('visible');
+          return false; // Deixa de ser um elemento a ser revelado
         }
+
+        return true; // Mantém sendo um elemento a ser revelado
       });
+
+      // Se não houver mais elementos para revelar, remove os listeners
+      if (!elementsToScroll.length) {
+        window.removeEventListener('scroll', smoothReveal);
+      }
     }
-    smoothReveal(); // Chama a função imediatamente para que ela não dependa do scroll inicial do usuário
+
+    smoothReveal(); // Chama a função para já ocorrer a primeira revelação sem necesistar do evento de scroll
     window.addEventListener('scroll', smoothReveal);
   }
 }
